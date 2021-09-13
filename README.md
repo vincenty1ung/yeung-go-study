cc2a9f6cb19b43c2d99e67f0de35346f8423c473
vincentyeung <vincent.yeung.bo@gmail.com>
2021-09-13 12:01:23 +0800

    feat(main): 结构体指针学习

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # go-study
-go语言学习：1.http调用 2. type func()探索 3.beego-logs应用
+## go语言学习：
+
+* http调用 
+* type func()探索 
+* beego-logs应用
+* 结构体指针副本传递及链表临位去重
+  ![st.png](structPHandler.jpeg)
+``
--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/astaxie/beego/logs"
 	"net/http"
 	"time"
+
+	"github.com/astaxie/beego/logs"
 )
 
-//创建log
+// 创建log
 var log = logs.NewLogger()
 
-//初始化，日志输出方式采用beego-logs study
+// 初始化，日志输出方式采用beego-logs study
 func init() {
-	//文件输出
-	//_ = logs.SetLogger(logs.AdapterFile, `{"filename":"project.log","level":7,"maxlines":0,"maxsize":0,"daily":true,"maxdays":10,"color":true}`)
-	//控制台输出
+	// 文件输出
+	// _ = logs.SetLogger(logs.AdapterFile, `{"filename":"project.log","level":7,"maxlines":0,"maxsize":0,"daily":true,"maxdays":10,"color":true}`)
+	// 控制台输出
 	_ = log.SetLogger(logs.AdapterConsole)
 
 }
@@ -26,31 +27,50 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// 函数式范式
+	handleFunc()
+	// 日志处理
+	logHandler()
+	// http处理
+	httpHandler()
+	// slice 指针 基础结构学习
+	slicePHandler()
+	// struct 指针 基础结构学习
+	structPHandler()
+
+}
+
+func handleFunc() {
 	man := Man{name: "zhangbo", age: 15, length: 13}
 	man.handleFunc(man.age, atLastHandleForAge)
 	man.handleFunc(man.length, loggingNumHandler(atLastHandleForLength))
-	//===================test====================
-	//an official log.Logger with prefix ORM
+}
+
+func httpHandler() {
+	// ===================test====================
+
+	// 创建一个新的http路由管理器
+	mux := http.NewServeMux()
+	mux.HandleFunc("/index", indexHandler)
+	// handler
+	mux.Handle("/hello", loggingHandler(&HtmlHandler{}))
+	// handler处理器
+	mux.HandleFunc("/hello1", hello)
+	// 只是监听8080端口
+	_ = http.ListenAndServe(":8080", mux)
+
+	// Clinet -> Requests ->  [Multiplexer(router) -> handler  -> Response -> Clinet
+}
+
+func logHandler() {
+	// ===================test====================
+	// an official log.Logger with prefix ORM
 	log.Info("======================")
 	log.Debug("my book is bought in the year of ", 2016)
 	log.Info("this %s cat is %v years old", "yellow", 3)
 	log.Warn("json is a type of kv like", map[string]int{"key": 2016})
 	log.Error("1024", "is a very", "good game")
 	log.Info("======================")
-	//===================test====================
-
-	//创建一个新的http路由管理器
-	mux := http.NewServeMux()
-	mux.HandleFunc("/index", indexHandler)
-	//handler
-	mux.Handle("/hello", loggingHandler(&HtmlHandler{}))
-	//handler处理器
-	mux.HandleFunc("/hello1", hello)
-	//只是监听8080端口
-	_ = http.ListenAndServe(":8080", mux)
-
-	//Clinet -> Requests ->  [Multiplexer(router) -> handler  -> Response -> Clinet
-
 }
 
 /*
@@ -61,7 +81,7 @@ type HtmlHandler struct {
 }
 
 func (htmlHandler *HtmlHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//声明响应的数据为html
+	// 声明响应的数据为html
 	w.Header().Set("Content-Type", "text/html")
 	logs.Info("设置响应头%s 为：%s", "Content-Type", "text/html")
 
@@ -83,7 +103,7 @@ func (htmlHandler *HtmlHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 	2.创建handler处理器
 */
 func hello(w http.ResponseWriter, r *http.Request) {
-	//声明响应的数据为html
+	// 声明响应的数据为html
 	w.Header().Set("Content-Type", "text/html")
 	logs.Info("设置响应头%s 为：%s", "Content-Type", "text/html")
 
@@ -105,19 +125,23 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	3.函数式范式
 */
 func middlewareHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 执行handler之前的逻辑
-		next.ServeHTTP(w, r)
-		// 执行完毕handler后的逻辑
-	})
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			// 执行handler之前的逻辑
+			next.ServeHTTP(w, r)
+			// 执行完毕handler后的逻辑
+		},
+	)
 }
 func loggingHandler(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		log.Info("开始请求，请求类型：%s 请求地址：%s 请求地址：%s", r.Method, r.URL.Path, r.Host)
-		next.ServeHTTP(w, r)
-		log.Info("请求完成，请求地址：%s 耗时：%v", r.URL.Path, time.Since(start))
-	})
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			log.Info("开始请求，请求类型：%s 请求地址：%s 请求地址：%s", r.Method, r.URL.Path, r.Host)
+			next.ServeHTTP(w, r)
+			log.Info("请求完成，请求地址：%s 耗时：%v", r.URL.Path, time.Since(start))
+		},
+	)
 
 	/*return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
@@ -127,14 +151,14 @@ func loggingHandler(next http.Handler) http.Handler {
 	}*/
 }
 
-//===========================================type func=================================================
+// ===========================================type func=================================================
 type Man struct {
 	name   string
 	age    int
 	length int
 }
 type NumHandler interface {
-	//最终处理at last
+	// 最终处理at last
 	handle(num int)
 }
 
@@ -142,7 +166,7 @@ type HandlerInt func(num int)
 
 func (handlerInt HandlerInt) handle(num int) {
 	handlerInt(num)
-	//handlerInt.handle(num)
+	// handlerInt.handle(num)
 }
 
 func atLastHandleForAge(age int) {
@@ -153,8 +177,8 @@ func atLastHandleForLength(length int) {
 }
 
 func (man Man) handleFunc(num int, atLastHandle func(num int)) {
-	//第二个参数是把传入的函数atLastHandle 强转成 HandlerInt类型，这样atLastHandle就实现了NumHandler接口。
-	//var handlerInt NumHandler  = HandlerInt(atLastHandle)
+	// 第二个参数是把传入的函数atLastHandle 强转成 HandlerInt类型，这样atLastHandle就实现了NumHandler接口。
+	// var handlerInt NumHandler  = HandlerInt(atLastHandle)
 	atLastCall(num, HandlerInt(atLastHandle))
 }
 func atLastCall(int int, numHandler NumHandler) {
@@ -172,12 +196,110 @@ func loggingNumHandler(handlerInt HandlerInt) HandlerInt {
 			log.Info("之后")
 		})
 	*/
-	//等价于函数
+	// 等价于函数
 	return func(num int) {
-		//之前
+		// 之前
 		log.Info("之前")
 		handlerInt.handle(num)
-		//之后
+		// 之后
 		log.Info("之后")
 	}
+}
+
+type nextNode struct {
+	i    int
+	next *nextNode
+}
+
+func slicePHandler() {
+	int32s := make([]int32, 0)
+	int32s = append(int32s, 1)
+	int32s = append(int32s, 2)
+	int32s = append(int32s, 3)
+	int32s = append(int32s, 4)
+	int32s = append(int32s, 5)
+	// p1 := &int32s
+	fmt.Println("p1:")
+	// fmt.Println(&p1)
+	// fmt.Println(fmt.Sprintf("%p", int32s))
+	// fmt.Println(&p1)
+	fmt.Println(fmt.Sprintf("%p", &int32s))
+	fmt.Println(fmt.Sprintf("len:%d", len(int32s)))
+	fmt.Println(fmt.Sprintf("cap:%d", cap(int32s)))
+	fmt.Println(int32s)
+	p1 := &int32s
+	fmt.Println(fmt.Sprintf("指针的地址1:%v,共同指向结构体地址%p", &p1, p1))
+	sfq(p1)
+	fmt.Println(fmt.Sprintf("len:%d", len(int32s)))
+	fmt.Println(fmt.Sprintf("cap:%d", cap(int32s)))
+	fmt.Println(int32s)
+}
+func sfq(list *[]int32) {
+	fmt.Println("p2:")
+	fmt.Println(fmt.Sprintf("指针的地址2:%v,共同指向结构体地址%p", &list, list))
+	// fmt.Println(&list)
+	*list = append(*list, 6)
+	*list = append(*list, 6)
+	*list = append(*list, 6)
+	*list = append(*list, 6)
+	fmt.Println(fmt.Sprintf("指针的地址2:%v,共同指向结构体地址%p", &list, list))
+	fmt.Println(fmt.Sprintf("len:%d", len(*list)))
+	fmt.Println(fmt.Sprintf("cap:%d", cap(*list)))
+}
+func sfq1(list []int32) {
+	fmt.Println("p2:")
+	// fmt.Println(&p2)
+	fmt.Println(fmt.Sprintf("p2:%p", &list))
+	list = list[2:4]
+	fmt.Println(list)
+	fmt.Println(fmt.Sprintf("len:%d", len(list)))
+	fmt.Println(fmt.Sprintf("cap:%d", cap(list)))
+	list = append(list, 7)
+	list = append(list, 6)
+	list = append(list, 8)
+	list = append(list, 9)
+	list = append(list, 10)
+	fmt.Println(fmt.Sprintf("p2:%p", &list))
+	fmt.Println(fmt.Sprintf("len:%d", len(list)))
+	fmt.Println(fmt.Sprintf("cap:%d", cap(list)))
+	fmt.Println(list)
+}
+
+/*
+!!!!!所有的传递传递的都是副本
+*/
+func structPHandler() {
+	// [1,2,2,3,4] 去重
+	node := nextNode{
+		i: 1, next: &nextNode{
+			i: 2, next: &nextNode{
+				i: 2, next: &nextNode{
+					i: 3, next: &nextNode{
+						4, nil,
+					},
+				},
+			},
+		},
+	}
+	fmt.Println(fmt.Sprintf("%+v", node))
+	// fmt.Println(&node)
+	p1 := &node
+	fmt.Println(fmt.Sprintf("指针的地址1:%v,共同指向结构体地址%p", &p1, p1))
+	handler(p1)
+	fmt.Println(fmt.Sprintf("%+v", node))
+}
+
+func handler(node *nextNode) {
+	fmt.Println(fmt.Sprintf("指针的地址2:%v,共同指向结构体地址%p", &node, node))
+	if node == nil {
+		return
+	}
+	for node.next != nil {
+		if node.i == node.next.i {
+			node.next = node.next.next
+		} else {
+			node = node.next
+		}
+	}
+	fmt.Println(node)
 }
